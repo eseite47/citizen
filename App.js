a79aa56b2755419ad2fd6e9d0b3cc7c1322f6fa9
Elisabeth Seite <elisabeth.seite@gmail.com>
2019-02-02 14:53:52 -0500

    transcribed civic test questions

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
@@ -7,10 +7,13 @@ export default class App extends React.Component {
       <Fragment>
         <View style={styles.container}>
           <Text style={styles.header}>Citizen</Text>
+          <Image style={styles.seal} source={require('./assets/Great_Seal_of_the_United_States.png')} />
         </View>
         <View style={styles.container}>
+          <Text>Let's study</Text>
           <Text>American Government</Text>
           <Text>History and Geography</Text>
+          <Text>Everything </Text>
         </View>
       </Fragment>
     );
@@ -19,14 +22,19 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   header: {
-    flex: 1,
+    marginTop: 40,
+    marginBottom: 30,
     backgroundColor: '#fff',
     fontSize: 40,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  seal: {
+
+    width: 200, 
+    height: 200
+  },
   container: {
-    flex: 2,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
