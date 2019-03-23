10ce20d642e9e9be6ae3e9144ec54253239a4e04
Elisabeth Seite <elisabeth.seite@gmail.com>
2019-03-23 13:11:58 -0400

    added peopleAnswer content to utils file

---
+++ utils/people.js
@@ -0,0 +1,31 @@
+const people = [
+  {
+    key: 'president',
+    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/946px-Donald_Trump_official_portrait.jpg',
+    title: 'President of the United States'
+  },{
+    key: 'vicePresident',
+    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Mike_Pence_official_Vice_Presidential_portrait.jpg/960px-Mike_Pence_official_Vice_Presidential_portrait.jpg',
+    title: 'Vice President of the United States'
+  },{
+    key: 'chiefJustice',
+    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Official_roberts_CJ.jpg/1024px-Official_roberts_CJ.jpg',
+    title: 'Chief Justice of the Supreme Cour of the United States'
+  },{
+    key: 'speaker',
+    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Nancy_Pelosi_2012.jpg/1024px-Nancy_Pelosi_2012.jpg',
+    title: 'Speaker of the House of Representatives'
+  },{
+    key: 'washington',
+    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg/1280px-Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg',
+    title: 'First President of the Unites States'
+  },{
+    key: 'wilson',
+    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/President_Wilson_1919-bw.tif/lossless-page1-1024px-President_Wilson_1919-bw.tif.png',
+    title: '28th President of the United States'
+  },{
+    key: 'roosevelt',
+    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/42/FDR_1944_Color_Portrait.jpg',
+    title: '32nd president of the United States '
+  }
+]
