3bcb7f888d317b260a6ec404647aa79c16bef61e
Elisabeth Seite <elisabeth.seite@gmail.com>
2019-11-23 17:36:00 -0500

    randomize order of questions

--- utils/getQuestions.js
+++ utils/getQuestions.js
@@ -1,8 +1,11 @@
-import {questions} from './questions'
+import { questions } from './questions';
+import randomizeOrder from './randomizeOrder';
 
 export default (category, type) => {
-  if (!category) return questions
-  return questions.filter(q => {
-      return q.category === category && (q.type === type || !type)
-  })
-}
+  if (!category) return questions;
+  const questionSet = questions.filter(q => {
+    return q.category === category && (q.type === type || !type);
+  });
+
+  return randomizeOrder(questionSet);
+};
